effacefb959947285946c442ff87308c5eb56774
Migelangel Carrillo <migelangelcarrillo2@gmail.com>
2025-03-01 12:12:49 -0800

    Progress Part 3, 03/01 @ afternoon

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-const helloWorld = document.getElementsByTagName("hello-world")[0]; 
-if (helloWorld) {
-    helloWorld.addEventListener("click", (e) => {
-        console.log("Hello World")
+document.addEventListener('DOMContentLoaded', () => {
+    let contactForm = document.getElementById('contact');
+    let name = document.getElementById('name');
+    let nameError = document.getElementById('error-message');
+    let email = document.getElementById('email');
+    let emailError = document.getElementById('error-email');
+    let message = document.getElementById('message');
+    let messageError = document.getElementById('error-message');
+    let messageCharCount = document.getElementById('char-count');
+    let possibleBot = document.getElementById('possible_bot');
+
+    message.addEventListener('input', () => {
+        const length = message.value.length
+        messageCharCount.textContent = length
+        
     })
-}
+
+        
+})
+    
